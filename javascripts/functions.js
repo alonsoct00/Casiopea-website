684e278cd573065299d3b8fc8392e591aceb16e8
Alonso CT <g.alonso.ct@gmail.com>
2026-04-26 11:23:25 -0600

    update site

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -7,9 +7,8 @@ Made by Themanoid
 */
 
 class Header extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML =
-            `
+  connectedCallback() {
+    this.innerHTML = `
             <section>
                 <a class='h-link brand' href='index.html'>
                     <span class="img-logo">
@@ -63,16 +62,15 @@ class Header extends HTMLElement {
                 </ul>
             </section>
                 
-         `
-    }
+         `;
+  }
 }
 
 //Footer
 
 class Footer extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML =
-            `
+  connectedCallback() {
+    this.innerHTML = `
                 <section class='text-center'>
                 <div class='social'>
                     <a class='icon fa fa-vimeo' href='https://vimeo.com/casiopea' target="_blank"></a>
@@ -86,139 +84,162 @@ class Footer extends HTMLElement {
                 </div>
             </section>
 
-    `
-
-    }
+    `;
+  }
 }
 
-customElements.define('main-header', Header);
-customElements.define('main-footer', Footer);
+customElements.define("main-header", Header);
+customElements.define("main-footer", Footer);
 
+(function ($) {
+  "use strict"; // Strict mode
 
-(function($) {
-
-    "use strict"; // Strict mode
-
-    /*
+  /*
         Portfolio scripts
     */
 
-    //  Define the portfolio grid
-    var $grid = $('#grid');
+  //  Define the portfolio grid
+  var $grid = $("#grid");
 
-    //  Show filter options on trigger click
-    $('#filter-trigger').on('tap click', function() {
-        $('#filter-trigger').fadeOut(200, function() {
-            $('#filters').fadeIn(500);
-        });
+  //  Show filter options on trigger click
+  $("#filter-trigger").on("tap click", function () {
+    $("#filter-trigger").fadeOut(200, function () {
+      $("#filters").fadeIn(500);
     });
+  });
 
+  //  On filter click, filter grid
+  $("#filters").on("tap click", "button", function (e) {
+    e.stopPropagation();
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+    $(".item").addClass("visible");
+    e.preventDefault();
+  });
 
-    //  On filter click, filter grid
-    $('#filters').on('tap click', 'button', function(e) {
-        e.stopPropagation();
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-        $('.item').addClass('visible');
-        e.preventDefault();
+  $(".filter-nav-anchor").each(function () {
+    var filterNav = $(this).attr("data-filter");
+    $(this).on("click", function (event) {
+      $("#casiopea-projects").find("#filter-trigger").trigger("tap");
+      $grid.isotope({ filter: filterNav });
+      $(".item").addClass("visible");
+      event.preventDefault();
+      //console.log(filterNav);
     });
+  });
 
-    $(".filter-nav-anchor").each(function() {
-        var filterNav = $(this).attr('data-filter');
-        $(this).on('click', function(event) {
-            $('#casiopea-projects').find('#filter-trigger').trigger('tap');
-            $grid.isotope({ filter: filterNav });
-            $('.item').addClass('visible');
-            event.preventDefault();
-            //console.log(filterNav);
-        });
-    });
+  // Back to top button
+  var $toTop = $('<div class="back-to-top"></div>');
+  $("body").append($toTop);
+  $("body").on("tap", ".back-to-top", function (e) {
+    $("html,body").animate({ scrollTop: 0 });
+    e.preventDefault();
+  });
 
+  //  Scroll effects
+  $(window).scroll(function () {
+    var scrolled = $(window).scrollTop();
+    var scrolledPercentage =
+      (100 - (scrolled / $(window).height()) * 100) / 100;
+    $(".jumbotron").css("opacity", scrolledPercentage);
+    if (scrolled > 200)
+      $toTop.addClass("active"); // Back to top button
+    else $toTop.removeClass("active");
+  });
 
+  $(window).load(function () {
+    $(".container-fluid").addClass("loaded"); // Initialize the container
+    $grid.isotope(); // Set the grid to isotope
 
-    // Back to top button
-    var $toTop = $('<div class="back-to-top"></div>');
-    $('body').append($toTop);
-    $('body').on('tap', '.back-to-top', function(e) {
-        $('html,body').animate({ scrollTop: 0 });
-        e.preventDefault();
-    });
+    $(".item").waypoint(
+      function () {
+        $(this).addClass("visible"); // Show items
+        $grid.isotope(); // Reload isotope items
+      },
+      { offset: "70%" },
+    );
 
-    //  Scroll effects
-    $(window).scroll(function() {
-        var scrolled = $(window).scrollTop();
-        var scrolledPercentage = (100 - (scrolled / $(window).height() * 100)) / 100;
-        $('.jumbotron').css('opacity', scrolledPercentage);
-        if (scrolled > 200)
-            $toTop.addClass('active'); // Back to top button
-        else
-            $toTop.removeClass('active');
-    });
+    $(".fadeIn").waypoint(
+      function () {
+        // Fade in every .fadeIn class element
+        $(this).addClass("visible");
+      },
+      { offset: "70%" },
+    );
 
-    $(window).load(function() {
-        $('.container-fluid').addClass('loaded'); // Initialize the container
-        $grid.isotope(); // Set the grid to isotope
+    var scrolled = $(window).scrollTop();
+    if (scrolled > 200) $toTop.addClass("active"); // Back to top button
 
-        $('.item').waypoint(function() {
-            $(this).addClass('visible'); // Show items
-            $grid.isotope(); // Reload isotope items
-        }, { offset: '70%' });
+    // Placeholder fix for older browsers
+    $("input, textarea").placeholder();
+  });
 
-        $('.fadeIn').waypoint(function() { // Fade in every .fadeIn class element
-            $(this).addClass('visible');
-        }, { offset: '70%' });
+  $("header").affix(); // Affix the header
 
-        var scrolled = $(window).scrollTop();
-        if (scrolled > 200)
-            $toTop.addClass('active'); // Back to top button
+  $(".trigger").on("tap", function (e) {
+    e.stopPropagation();
+    $("#navigation").toggleClass("active"); // Toggle responsive menu
+  });
 
-        // Placeholder fix for older browsers
-        $('input, textarea').placeholder();
-    });
+  $("html").on("tap", function () {
+    // Used to hide the responsive navigation on click outside
+    $("#navigation").removeClass("active");
+  });
 
-    $('header').affix(); // Affix the header
+  // Fade effect on navigation / header links
+  $("a.h-link").on("tap", function (e) {
+    e.stopPropagation();
+    var href = $(this).attr("href");
+    if (
+      href != "#" &&
+      !$(this).hasClass("lightbox") &&
+      !$(this).hasClass("anchor")
+    ) {
+      $("body").fadeOut(400, function () {
+        window.location = href; // Go to url after smooth transition
+      });
+      e.preventDefault();
+    }
+    if ($(this).hasClass("anchor")) {
+      var href = $(this).attr("href");
+      $("html,body").animate(
+        {
+          scrollTop: $(href).offset().top - 50 + "px",
+        },
+        800,
+      );
+      $("#navigation").removeClass("active");
+      e.preventDefault();
+    }
+  });
 
-    $('.trigger').on('tap', function(e) {
-        e.stopPropagation();
-        $('#navigation').toggleClass('active'); // Toggle responsive menu
-    });
+  //Fixes Firefox back button issue
+  $(window).bind("unload", function () {
+    // Nothing needed here :-)
+  });
 
-    $('html').on('tap', function() {
-        // Used to hide the responsive navigation on click outside
-        $('#navigation').removeClass('active');
-    });
+  $(window).bind("pageshow", function (event) {
+    if (event.originalEvent.persisted) {
+      window.location.reload();
+    }
+  });
 
-    // Fade effect on navigation / header links
-    $('a.h-link').on('tap', function(e) {
-        e.stopPropagation();
-        var href = $(this).attr('href');
-        if (href != '#' && !$(this).hasClass('lightbox') && !$(this).hasClass('anchor')) {
-            $('body').fadeOut(400, function() {
-                window.location = href; // Go to url after smooth transition
-            });
-            e.preventDefault();
-        }
-        if ($(this).hasClass('anchor')) {
-            var href = $(this).attr('href');
-            $('html,body').animate({
-                scrollTop: ($(href).offset().top) - 50 + 'px'
-            }, 800);
-            $('#navigation').removeClass('active');
-            e.preventDefault();
-        }
-    })
-
-    //Fixes Firefox back button issue
-    $(window).bind("unload", function() {
-        // Nothing needed here :-)
-    });
-
-    $(window).bind("pageshow", function(event) {
-        if (event.originalEvent.persisted) {
-            window.location.reload()
-        }
-    });
-
-    $('<a href="/proyectos.html" id="back-cta">Regresar</a>').insertBefore('.other-projects-block');
-
+  $('<a href="/proyectos.html" id="back-cta">Regresar</a>').insertBefore(
+    ".other-projects-block",
+  );
 })(jQuery);
+
+document.addEventListener("keydown", (e) => {
+    const isCmdOrCtrl = e.metaKey || e.ctrlKey;
+  if (isCmdOrCtrl && (e.key === "+" || e.key === "-" || e.key === "=")) {
+    e.preventDefault();
+  }
+});
+document.addEventListener(
+  "wheel",
+  (e) => {
+    const isCmdOrCtrl = e.metaKey || e.ctrlKey;
+    if (isCmdOrCtrl) e.preventDefault();
+  },
+  { passive: false },
+);
